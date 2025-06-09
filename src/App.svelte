066798b6131e086ca8c54e87204ad118d63a20de
AllieBaig <136852901+AllieBaig@users.noreply.github.com>
2025-06-09 11:28:09 +0100

    Create App.svelte

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+
+<script>
+  import MixLingo from './modes/MixLingo.svelte';
+</script>
+
+<main>
+  <h1>lingoq2 🌍</h1>
+  <MixLingo />
+</main>
+
+<style>
+  main {
+    font-family: sans-serif;
+    text-align: center;
+    padding: 2rem;
+  }
+</style>
